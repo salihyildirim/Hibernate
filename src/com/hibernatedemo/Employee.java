4fbb5996903deacf96bc9e7749e66a55d300f04d
Salih <salihyildirimce@gmail.com>
2023-08-08 17:19:42 +0300

    cfg

---
+++ src/com/hibernatedemo/Employee.java
@@ -0,0 +1,52 @@
+package com.hibernatedemo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @Column
+    private int ID;
+    @Column
+    private int countryID;
+    @Column
+    private String name;
+    @Column
+    private int age;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(int countryID) {
+        this.countryID = countryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/com/hibernatedemo/Main.java
+++ src/com/hibernatedemo/Main.java
@@ -4,30 +4,35 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(City.class)
                 .buildSessionFactory();
 
+
         Session session = factory.getCurrentSession();
 
         try {
             session.beginTransaction();
+            // HQL --> Hibernate Query Language // DİKKAT SELECT YOK. ÇÜNKÜ TÜM City dönüyor. Foreach ile istediğin alanı select gibi seçersin sonra.
+            // from City c where c.countryCode='TUR' OR countryCode='EN' OR c.name LIKE '%F%'
+            List<City> cities = session.createQuery("select c.countryCode  from City c group by c.countryCode ").getResultList();
 
-           List<City> cities = session.createQuery("from City c where c.countryCode='TUR'").getResultList();
-
-           for(City city:cities){
-               System.out.println(city.getName());
-           }
+            for (City city : cities) {
+                System.out.println(city.getName());
+            }
 
             session.getTransaction().commit();
-        }finally{
+        } finally {
             factory.close();
         }
+    }
+
+    public <T> void denemee(T x) {
+
     }
 }
