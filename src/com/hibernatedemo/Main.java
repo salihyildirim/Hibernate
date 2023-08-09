5a9e20ce899e9182804a8ac90525db2c8ed39910
Salih <salihyildirimce@gmail.com>
2023-08-09 10:34:07 +0300

    CRUD

--- src/com/hibernatedemo/Main.java
+++ src/com/hibernatedemo/Main.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,17 +13,40 @@ public class Main {
                 .buildSessionFactory();
 
 
+        // session Unit Of Work tasarım desenidir.
         Session session = factory.getCurrentSession();
 
         try {
             session.beginTransaction();
             // HQL --> Hibernate Query Language // DİKKAT SELECT YOK. ÇÜNKÜ TÜM City dönüyor. Foreach ile istediğin alanı select gibi seçersin sonra.
-            // from City c where c.countryCode='TUR' OR countryCode='EN' OR c.name LIKE '%F%'
-            List<City> cities = session.createQuery("select c.countryCode  from City c group by c.countryCode ").getResultList();
+            //List<City> cities =  from City c where c.countryCode='TUR' OR countryCode='EN' OR c.name LIKE '%F%'
+            //INSERT
+//            City city = new City();
+//            city.setDistrict("kenya");
+//            city.setName("KENYA");
+//            city.setCountryCode("KEN");
+//            city.setId(10);
+//            city.setPopulation(650);
+//            session.save(city);
 
-            for (City city : cities) {
-                System.out.println(city.getName());
-            }
+            //UPDATE
+//            City city = session.get(City.class,0);
+//            city.setPopulation(1511);
+//            session.save(city);
+
+            //DELETE
+//            City city = session.get(City.class,0);
+//            session.delete(city);
+
+            // Session, Unit Of Work olduğu için bir save'de hata varsa hepsini iptal eder.
+
+            //List<Integer> populations = session.createQuery("select c.population from City c group by c.population ").getResultList();
+
+//            List<City> populations = session.createQuery(" from City c where c.countryCode='TUR' OR countryCode='EN' OR c.name LIKE '%F%'").getResultList(); // .executeUpdate(); bir update delete sorgusu yazarsan.
+
+//            for (City population : populations) {
+//                System.out.println(population.getName());
+//            }
 
             session.getTransaction().commit();
         } finally {
